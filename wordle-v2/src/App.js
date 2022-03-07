7effc4f6ab0614b2ac4dae5e2f4a144464ba2654
Timothy <timhobson@live.co.uk>
2022-03-07 21:14:09 +0000

    bunch of stuff

--- wordle-v2/src/App.js
+++ wordle-v2/src/App.js
@@ -4,7 +4,7 @@ import WORDS from './words/words';
 
 function App() {
   function createTargetWord(words) {
-    return words[3];
+    return words[3].toUpperCase().split('');
   }
   const target = createTargetWord(WORDS);
 
@@ -13,12 +13,67 @@ function App() {
 
   const [location, setLocation] = useState({ x: 1, y: 1 })
 
-  const handleCompeteRow = (e) => {
+  const getRowSubmission = (e) => {
+    console.log(e);
+    let cell = e.target.parentElement.firstChild;
+    let submission = "";
+    let counter = 1;
+    while (counter <= row_len) {
+      submission += cell.value;
+      counter += 1;
+      cell = cell.nextElementSibling;
+    }
+    return submission.toUpperCase();
+  }
+
+  const getMatches = (submission) => {
+    let matches = [];
+    for (let i = 0; i < row_len; i++) {
+      let letter = submission[i];
+      if (letter === target[i]) {
+        matches.push('green');
+      }
+      else if (
+        target.includes(letter)
+        && !submission.slice(0, i).includes(letter)
+      ) {
+        matches.push('yellow');
+      }
+      else {
+        matches.push('grey');
+      }
+    }
+    return matches;
+  }
+
+  const formatRowBasedOnMatches = (e, matches) => {
+    let cell = e.target.parentElement.firstChild;
+    let counter = 0;
+    while (counter < row_len) {
+      cell.className = matches[counter] + '_tile';
+      cell = cell.nextElementSibling;
+      counter += 1;
+    }
+  }
+
+  const handleCompleteRow = (e) => {
     console.log("Handling complete row.");
+    let rowSubmission = getRowSubmission(e);
+    console.log(rowSubmission);
+    if (WORDS.includes(rowSubmission)) {
+      let matches = getMatches(rowSubmission.split(''));
+      console.log(matches);
+
+      formatRowBasedOnMatches(e, matches);
+      return true;
+    }
+    else {
+      alert("Not a valid word!");
+      return false;
+    }
   }
 
   const handleTextChange = (e, loc) => {
-    console.log(e);
     if (e.nativeEvent.inputType === 'deleteContentBackward') {
       // do nothing  
     }
@@ -26,16 +81,23 @@ function App() {
       e.target.nextElementSibling.focus();
       setLocation({ x: loc.x + 1, y: loc.y });
     }
+    else if (loc.x === row_len) {
+      setLocation({ x: loc.x + 1, y: loc.y });
+    }
   }
 
   const handleKeyDown = (e, loc) => {
-    if (e.code === "Backspace" && loc.x > 1) {
-      setLocation({ x: loc.x - 1, y: loc.y });
-      e.target.previousSibling.focus();
+    if (e.code === "Backspace") {
+      if (loc.x > 1 && loc.x <= row_len) {
+        setLocation({ x: loc.x - 1, y: loc.y });
+        e.target.previousSibling.focus();
+      }
+      else if (loc.x > row_len) {
+        setLocation({ x: loc.x - 1, y: loc.y });
+      }
     }
-    if (e.code === "Enter" && loc.x > row_len) {
-      handleCompeteRow(e);
-      if (loc.y < col_len) {
+    else if (e.code === "Enter" && loc.x > row_len) {
+      if (loc.y < col_len && handleCompleteRow(e)) {
         console.log(e.target.parentElement.nextElementSibling.firstChild.focus());
         setLocation({ x: 1, y: loc.y + 1 });
       }
